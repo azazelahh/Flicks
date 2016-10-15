4fb76fdcfa7d876875218bf40ede21e633448845
Olya Sorokina <Olya@olyas-macbook-air.local>
2016-10-14 19:58:36 -0700

    Initial Commit

---
+++ Flicks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flicks
+//
+//  Created by Olya Sorokina on 10/14/16.
+//  Copyright © 2016 olya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
